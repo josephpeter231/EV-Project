760f13b9a1608390c9a7bfad22cf057e5501674f
josephpeter231 <josephpeterjcit@gmail.com>
2024-05-19 15:52:09 +0530

    tile updated

--- src/components/AdminMap.jsx
+++ src/components/AdminMap.jsx
@@ -18,7 +18,7 @@ const MapIntegration = () => {
   useEffect(() => {
     const map = L.map('map').setView([13.0827, 80.2707], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROGRvCr4nBwXxZAyYEmdfCevT5jxB4GhEfrw&s', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
