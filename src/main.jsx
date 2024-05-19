a185b9ba781e3b5879e2021c499586eeba809ea5
josephpeter231 <josephpeterjcit@gmail.com>
2024-05-19 14:45:55 +0530

    Changed browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
+  
   
   
 )
